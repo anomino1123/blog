4d0163c9cded31631ddfe5d46c0667af74969b58
anomino1123 <henriqueramosp822@gmail.com>
2026-05-29 17:41:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'reporter-periferia-v1';
-const urlsToCache = ['', 'index.html', 'login.html', 'register.html', 'perfil.html', 'manifest.json', 'style.css', 'database.js', '/notifications.js', 'auth.js', 'mobile.js', 'app.js'];
+const urlsToCache = ['', 'index.html', 'login.html', 'register.html', 'perfil.html', 'manifest.json', 'style.css', 'database.js', 'notifications.js', 'auth.js', 'mobile.js', 'app.js'];
 
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
 self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))); });
